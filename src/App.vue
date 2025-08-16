0bcbf2ba0ad8c8578d9f17201dd0aeb958a74ee1
LSE-NZ <luk.fymb@gmail.com>
2025-08-16 07:39:42 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Stats Grabber</h1>
+    <button @click="testConnection">Test Backend Connection</button>
+    
+    <div v-if="loading">Loading...</div>
+    <div v-if="error" style="color: red;">Error: {{ error }}</div>
+    <div v-if="data">
+      <h3>Backend Response:</h3>
+      <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import { fetchData } from './services/api.js';
+
+export default {
+  data() {
+    return {
+      data: null,
+      loading: false,
+      error: null
+    };
+  },
+  methods: {
+    async testConnection() {
+      this.loading = true;
+      this.error = null;
+      this.data = null;
+      
+      try {
+        this.data = await fetchData();
+      } catch (error) {
+        this.error = error.message;
+      } finally {
+        this.loading = false;
+      }
+    }
+  }
+};
+</script>
